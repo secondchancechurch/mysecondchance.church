ed5f56f06ac00f97f438c4eff5907655ec74225e
Edolyne Long <ed@edolyne.com>
2019-08-27 13:18:39 -0400

    Changing build steps and included files

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 module.exports = {
   target: 'serverless',
   env: {
-    'GRAPHQL_ENDPOINT': process.env.GRAPHQL_ENDPOINT
+    'GRAPHQL_ENDPOINT': process.env.GRAPHQL_ENDPOINT,
+    'FONT_AWESOME_TOKEN': process.env.FONT_AWESOME_TOKEN
   }
 }
