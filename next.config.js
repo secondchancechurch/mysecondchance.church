6d22b90a81e74e1a7553875dda30d8301066708a
Edolyne Long <ed@edolyne.com>
2019-08-27 13:30:36 -0400

    Changing npmrc to env variable

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
   target: 'serverless',
   env: {
     'GRAPHQL_ENDPOINT': process.env.GRAPHQL_ENDPOINT,
-    'FONT_AWESOME_TOKEN': process.env.FONT_AWESOME_TOKEN
+    'NPM_RC': process.env.NPM_RC
   }
 }
